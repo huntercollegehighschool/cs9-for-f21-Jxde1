67ec75fe0698f7258e2cfcf65562c81426d5f554
Jade Li <94186716+Jxde1@users.noreply.github.com>
2021-12-17 16:24:18 +0000

    I completed the assignment by editing and writing code.

--- part4.py
+++ part4.py
@@ -40,3 +40,15 @@ fizz
 """
 
 # write your code here 
+number = int(input("Enter a number: "))
+
+for i in range(1, number + 1):
+  if i % 3 == 0 and i % 5 == 0:
+    print("fizzbuzz")
+  elif i % 5 == 0:
+    print("buzz")
+  elif i % 3 == 0:
+    print("fizz")
+  else:
+    print(i)
+
